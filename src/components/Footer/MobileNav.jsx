2ef096b59300f03428e7f640631ac7e6b54fcb1b
Pugga <aezakmi2508@gmail.com>
2022-08-25 03:35:13 +0300

    .

--- src/components/Footer/MobileNav.jsx
+++ src/components/Footer/MobileNav.jsx
@@ -11,13 +11,13 @@ const MobileNav = () => {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            fill="none"
+            fill="#181A43"
             xmlns="http://www.w3.org/2000/svg"
           >
             <g clip-path="url(#clip0_964_3872)">
               <path
                 d="M12 5.69L17 10.19V18H15V12H9V18H7V10.19L12 5.69ZM12 3L2 12H5V20H11V14H13V20H19V12H22L12 3Z"
-                fill="#181A43"
+                
               />
             </g>
             <defs>
